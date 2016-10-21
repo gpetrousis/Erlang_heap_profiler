8a671ff7090a9ace0f1e7afd2e4c5ebb5159a883
Georgios Petrousis <gpetrousis@gmail.com>
2016-10-21 15:29:55 +0200

    Implemented polling profiling

--- src/profiler.erl
+++ src/profiler.erl
@@ -1,15 +1,44 @@
 -module(profiler).
--export([profiler_start/0, profiler_stop/0]).
+-export([start/0, stop/0, polling_start/0, polling_stop/1]).
 
-profiler_start() ->
+-define(interval, 1000).
+
+polling_start() ->
+	spawn(fun () -> polling_start([]) end).
+
+polling_start(L) -> 
+	Procs = erlang:processes(),
+	Result = [poll_func(X) || X <- Procs],
+	receive
+		stop -> 
+			Output = jsx:encode(L),
+			file:write_file("dump.json", Output)
+	after 
+		?interval -> 
+			polling_start(Result++L)
+	end.
+
+polling_stop(Pid) ->
+	Pid ! stop.
+
+poll_func(Pid) ->
+	{_,Data} = erlang:process_info(Pid, garbage_collection_info),
+	{_, _, Ohs, Hs} = parse_trace(Data),
+	[{<<"pid">>, list_to_binary(pid_to_list(Pid))}, 
+	 {<<"type">>, <<"minor">>},
+	 {<<"old_heap_size">>, Ohs},
+	 {<<"heap_size">>, Hs},
+	 {<<"timestamp">>, erlang:convert_time_unit(erlang:monotonic_time(), native, millisecond) + erlang:time_offset(millisecond)}].
+
+
+start() ->
 	dbg:tracer(port, dbg:trace_port(file, "/tmp/trace.dmp")),
 	dbg:p(processes, [garbage_collection, monotonic_timestamp]),
 	ok.
 
-
-profiler_stop() ->
+stop() ->
 	dbg:stop_clear(),
-	Pid = dbg:trace_client(file, "/tmp/trace.dmp", {fun handler/2, []}),
+	dbg:trace_client(file, "/tmp/trace.dmp", {fun handler/2, []}),
 	ok.
 
 handler(end_of_trace, Return) -> 
@@ -32,8 +61,8 @@ parse_trace([X|Xs], {Ohbs, Hbs, Ohs, Hs}) ->
 
 
 
-parse({trace_ts, Pid, Type, L, Timestamp}) ->
-	{Ohbs, Hbs, Ohs, Hs} = parse_trace(L),
+parse({trace_ts, Pid, _, L, Timestamp}) ->
+	{_, _, Ohs, Hs} = parse_trace(L),
 	[{{<<"pid">>, list_to_binary(pid_to_list(Pid))}, 
 	 {<<"type">>, <<"minor">>},
 	 {<<"old_heap_size">>, Ohs},
