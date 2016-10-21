3144f58d11e192b6691b5236558864098b4b2a13
Lukas Larsson <lukas@erlang-solutions.com>
2016-10-21 17:30:38 +0200

    Fix some profiler stuff

--- src/profiler.erl
+++ src/profiler.erl
@@ -1,20 +1,27 @@
 -module(profiler).
 -export([start/0, stop/0, polling_start/0, polling_stop/1]).
 
--define(interval, 1000).
+-define(interval, 5).
 
 polling_start() ->
-	spawn(fun () -> polling_start([]) end).
+	spawn(fun () ->
+                      dbg:tracer(port, dbg:trace_port(file, "/tmp/trace.dmp")),
+                      dbg:p(all, [garbage_collection, monotonic_timestamp]),
+                      polling_start([])
+              end).
 
 polling_start(L) -> 
 	Procs = erlang:processes() -- [self()],
-	dbg:tracer(port, dbg:trace_port(file, "/tmp/trace.dmp")),
-	dbg:p(Procs, [garbage_collection, monotonic_timestamp]),
-	Result = [poll_func(X) || X <- Procs],
+	Result = lists:flatmap(fun poll_func/1,Procs),
+        erlang:display({sampling,erlang:system_time(millisecond)}),
 	receive
 		stop -> 
+                        erlang:display({dump,erlang:system_time(millisecond)}),
 			dbg:stop_clear(),
-			dbg:trace_client(file, "/tmp/trace.dmp", {fun handler/2, L})
+			Pid = dbg:trace_client(file, "/tmp/trace.dmp",
+                                               {fun handler/2, Result}),
+                        erlang:monitor(process, Pid),
+                        receive M -> M end
 	after 
 		?interval -> 
 			polling_start(Result++L)
@@ -22,17 +29,23 @@ polling_start(L) ->
 
 polling_stop(Pid) ->
 	Pid ! stop,
-
+        erlang:monitor(process, Pid),
+        receive M -> M end,
 	ok.
 
 poll_func(Pid) ->
-	{_,Data} = erlang:process_info(Pid, garbage_collection_info),
-	{_, _, Ohs, Hs} = parse_trace(Data),
-	[{<<"pid">>, list_to_binary(pid_to_list(Pid))}, 
-	 {<<"type">>, <<"poll">>},
-	 {<<"old_heap_size">>, Ohs},
-	 {<<"heap_size">>, Hs},
-	 {<<"timestamp">>, erlang:convert_time_unit(erlang:monotonic_time(), native, millisecond) + erlang:time_offset(millisecond)}].
+	case erlang:process_info(Pid, garbage_collection_info) of
+            undefined -> [];
+            {_,Data} ->
+                {_, _, Ohs, Hs} = parse_trace(Data),
+                [[{<<"pid">>, list_to_binary(pid_to_list(Pid))}, 
+                  {<<"type">>, <<"poll">>},
+                  {<<"old_heap_size">>, Ohs},
+                  {<<"heap_size">>, Hs},
+                  {<<"timestamp">>, erlang:convert_time_unit(
+                                      erlang:monotonic_time(), native, millisecond)
+                   + erlang:time_offset(millisecond)}]]
+        end.
 
 
 start() ->
