0dfc365e24d57fdaf8de14db19f8987e9092e7bd
Lukas Larsson <lukas@erlang-solutions.com>
2016-10-21 11:21:45 +0200

    Added test json

--- src/heap_profiler_handler.erl
+++ src/heap_profiler_handler.erl
@@ -1,6 +1,28 @@
 -module(heap_profiler_handler).
 -export([init/2]).
 
+init(Req0, json) ->
+    TestJson = <<"[
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 1 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 123, timestamp : 2 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 3 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 12342, timestamp : 4 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 5 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 12, timestamp : 6 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 7 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 123, timestamp : 8 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 9 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 12, timestamp : 10 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 12342, timestamp : 11 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 123, timestamp : 12 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 123, timestamp : 13 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 123, timestamp : 14 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 15 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 16 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 123, timestamp : 17 },
+     { pid : \"<0.1.0>\", type : minor, heap_size : 1234, timestamp : 18 }
+   ]">>,
+   TestJson;
 init(Req0, State) ->
 	Req = cowboy_req:reply(200,
 		#{<<"content-type">> => <<"text/plain">>},
